17fefc386c61a60c87da3896434a720bd5fda29b
In Hwa Na <n10851879@qut.edu.au>
2025-09-25 23:09:01 +1000

    Minor Bug Fixes

--- index.js
+++ index.js
@@ -16,15 +16,17 @@ s3Client = new S3.S3Client({ region: 'ap-southeast-2'})
 
 app.post('/upload', async (req,res)=>{
     // Return Upload Presigned URL
-    const {filename, contentType} = req.body
+    const {filename} = req.body
+    //const {filename, contentType} = req.body
     try {
         const command = new S3.PutObjectCommand({
                 Bucket: bucketName,
                 Key: filename,
-                ContentType: contentType
+                //ContentType: contentType
             });
         const presignedURL = await S3Presigner.getSignedUrl(s3Client, command, {expiresIn: 3600} );
         console.log(presignedURL);
+        //console.log("Received:", filename, contentType);
         res.json({url :presignedURL})
     } catch (err) {
         console.log(err);
