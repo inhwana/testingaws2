82b3d00ebcfb112947bf82b6c995d935828b7b90
In Hwa Na <n10851879@qut.edu.au>
2025-09-26 18:47:55 +1000

    Testing FFMPEG with webm

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post('/transcode', async (req,res) =>{
     // .outputFormat('webm')
     // .videoCodec('libvpx') // libvpx-vp9 For higher CPU Usage
     // .audioCodec('libvorbis') // libopus  
-    // .output('-')
+    .output('-')
     .on('start', cmd => console.log('FFmpeg started:', cmd))
     .on('error', (err) => {
     console.error('Error:', err.message);
@@ -81,6 +81,9 @@ app.post('/transcode', async (req,res) =>{
     })
     .on('end', () => console.log('FFmpeg finished'))
     .pipe(videostream, {end:true})
+
+
+
     // ffmpeg(video)
     // .videoCodec('libvpx')
     // .audioCodec('libvorbis')
